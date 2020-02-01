7b9ea5bd217bd1b1c4ed292c9bfa9a0841f90c1f
kamikaze.zirou <kamikaze.zirou@gmail.com>
2020-02-02 01:40:29 +0900

    feat: add screen when device isn't be registered

--- app/src/main/java/com/kamikaze/shareddevicemanager/ui/main/mydevice/MyDeviceViewModel.kt
+++ app/src/main/java/com/kamikaze/shareddevicemanager/ui/main/mydevice/MyDeviceViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class MyDeviceViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is my device Fragment"
+        value = "Your device is not registered"
     }
     val text: LiveData<String> = _text
 }
