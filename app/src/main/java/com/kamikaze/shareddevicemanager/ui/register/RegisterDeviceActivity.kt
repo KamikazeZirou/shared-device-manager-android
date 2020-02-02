fc6335d7e4a4b5b06eea7ab881098a121192182b
kamikaze.zirou <kamikaze.zirou@gmail.com>
2020-02-02 13:01:19 +0900

    feat: add actionbar of register device screen

--- app/src/main/java/com/kamikaze/shareddevicemanager/ui/register/RegisterDeviceActivity.kt
+++ app/src/main/java/com/kamikaze/shareddevicemanager/ui/register/RegisterDeviceActivity.kt
@@ -1,19 +1,21 @@
 package com.kamikaze.shareddevicemanager.ui.register
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.kamikaze.shareddevicemanager.R
+
 
 class RegisterDeviceActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_device)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, RegisterDeviceFragment.newInstance())
                 .commitNow()
         }
     }
-
 }
--- app/src/main/java/com/kamikaze/shareddevicemanager/ui/register/RegisterDeviceFragment.kt
+++ app/src/main/java/com/kamikaze/shareddevicemanager/ui/register/RegisterDeviceFragment.kt
@@ -1,11 +1,10 @@
 package com.kamikaze.shareddevicemanager.ui.register
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.kamikaze.shareddevicemanager.R
 
 class RegisterDeviceFragment : Fragment() {
@@ -27,6 +26,29 @@ class RegisterDeviceFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(RegisterDeviceViewModel::class.java)
         // TODO: Use the ViewModel
+
+        setHasOptionsMenu(true)
+        (activity as? AppCompatActivity)?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.register_device_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                activity?.finish()
+                return true
+            }
+            R.id.register_device -> {
+                // TODO Register Device
+                activity?.finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
