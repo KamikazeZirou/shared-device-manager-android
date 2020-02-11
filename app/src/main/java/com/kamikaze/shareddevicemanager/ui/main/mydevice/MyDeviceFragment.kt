c604c649f98d76a2dfda768cf99302f03062fa10
kamikaze.zirou <kamikaze.zirou@gmail.com>
2020-02-11 23:10:34 +0900

    feat: refine the screen of my device

--- app/src/main/java/com/kamikaze/shareddevicemanager/ui/main/mydevice/MyDeviceFragment.kt
+++ app/src/main/java/com/kamikaze/shareddevicemanager/ui/main/mydevice/MyDeviceFragment.kt
@@ -2,10 +2,9 @@ package com.kamikaze.shareddevicemanager.ui.main.mydevice
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.kamikaze.shareddevicemanager.R
 import com.kamikaze.shareddevicemanager.databinding.FragmentMyDeviceBinding
 import com.kamikaze.shareddevicemanager.ui.register.RegisterDeviceActivity
@@ -29,29 +28,52 @@ class MyDeviceFragment : DaggerFragment() {
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
-        //
-        // Not Registered Screen
-        //
         binding.buttonRegisterDevice.setOnClickListener {
             val intent = Intent(activity, RegisterDeviceActivity::class.java)
             startActivity(intent)
         }
 
-        // Registered Screen
-        binding.borrowButton.setOnClickListener {
-            val intent = Intent(activity, BorrowDeviceActivity::class.java)
-            startActivity(intent)
-        }
+        viewModel.deviceRegistered.observe(viewLifecycleOwner, Observer {
+            activity!!.invalidateOptionsMenu()
+        })
 
-        binding.returnButton.setOnClickListener {
-            viewModel.returnDevice()
-        }
-
-        binding.unregisterButton.setOnClickListener {
-            // TODO 確認ダイアログを出して、OKなら登録を消す
-            viewModel.unregister()
-        }
+        viewModel.isDeviceFree.observe(viewLifecycleOwner, Observer {
+            activity!!.invalidateOptionsMenu()
+        })
 
         return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        if (viewModel.deviceRegistered.value == true) {
+            inflater.inflate(R.menu.my_device_menu, menu)
+            menu.findItem(R.id.return_device).isVisible = (viewModel.isDeviceFree.value == false)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.borrow_device -> {
+                val intent = Intent(activity, BorrowDeviceActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.return_device -> {
+                viewModel.returnDevice()
+                true
+            }
+            R.id.dispose_device -> {
+                viewModel.unregister()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
