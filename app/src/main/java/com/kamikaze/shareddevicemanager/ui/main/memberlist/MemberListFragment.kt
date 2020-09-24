efcc8405e573b56bd6524f732050e1af3005166f
Zirou Kamikaze <kamikaze.zirou@gmail.com>
2020-09-25 00:29:00 +0900

    feat: メンバー一覧画面のMenuを追加

--- app/src/main/java/com/kamikaze/shareddevicemanager/ui/main/memberlist/MemberListFragment.kt
+++ app/src/main/java/com/kamikaze/shareddevicemanager/ui/main/memberlist/MemberListFragment.kt
@@ -1,11 +1,10 @@
 package com.kamikaze.shareddevicemanager.ui.main.memberlist
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import com.kamikaze.shareddevicemanager.R
+import com.kamikaze.shareddevicemanager.ui.common.openPrivacyPolicy
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,6 +35,32 @@ class MemberListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_member_list, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.member_list_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.sign_out -> {
+                TODO()
+                true
+            }
+            R.id.privacy_policy -> {
+                openPrivacyPolicy()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 
     companion object {
