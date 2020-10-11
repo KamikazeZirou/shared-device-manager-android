79e93ff3d43a255feaf523b3f8d77a0609a91c39
Zirou Kamikaze <kamikaze.zirou@gmail.com>
2020-10-12 01:22:50 +0900

    feat: グループ一覧画面の大まかなレイアウトを決めた

--- app/src/main/java/com/kamikaze/shareddevicemanager/ui/groups/GroupsFragment.kt
+++ app/src/main/java/com/kamikaze/shareddevicemanager/ui/groups/GroupsFragment.kt
@@ -1,12 +1,13 @@
 package com.kamikaze.shareddevicemanager.ui.groups
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kamikaze.shareddevicemanager.R
+import com.kamikaze.shareddevicemanager.model.data.Group
+import kotlinx.android.synthetic.main.fragment_groups.*
 
 class GroupsFragment : Fragment() {
 
@@ -16,6 +17,12 @@ class GroupsFragment : Fragment() {
 
     private lateinit var viewModel: GroupsViewModel
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(GroupsViewModel::class.java)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,10 +30,25 @@ class GroupsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_groups, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(GroupsViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        list.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = GroupsAdapter(viewModel).apply {
+                submitList(
+                    listOf(
+                        Group("1", "group1"),
+                        Group("2", "group2"),
+                        Group("3", "group3"),
+                    )
+                )
+            }
+        }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.groups_menu, menu)
+    }
 }
