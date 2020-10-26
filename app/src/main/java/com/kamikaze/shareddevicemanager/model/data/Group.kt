56f7f24e9e893e7837619c2ebf202fb645f32712
Zirou Kamikaze <kamikaze.zirou@gmail.com>
2020-10-26 23:25:19 +0900

    feat: Group削除機能のUIを実装

--- app/src/main/java/com/kamikaze/shareddevicemanager/model/data/Group.kt
+++ app/src/main/java/com/kamikaze/shareddevicemanager/model/data/Group.kt
@@ -1,9 +1,12 @@
 package com.kamikaze.shareddevicemanager.model.data
 
+import android.os.Parcelable
 import com.google.firebase.firestore.DocumentId
 import com.google.firebase.firestore.IgnoreExtraProperties
+import kotlinx.android.parcel.Parcelize
 
 @IgnoreExtraProperties
+@Parcelize
 data class Group(
     @DocumentId val id: String? = null,
     val name: String? = null,
@@ -11,4 +14,4 @@ data class Group(
     // FIXME Firestoreを使う都合上、存在するフィールドなので隠蔽したい
     val members: List<String>? = null,
     val default: Boolean? = null
-)
+) : Parcelable
